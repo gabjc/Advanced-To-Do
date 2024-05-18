a9128c3126ab0cf524d911d0a7d2464cd58bb4cb
Rohan Prasad <r.prasad@ufl.edu>
2024-05-18 17:58:02 -0400

    Basic Functions for user

--- backend/server/Models/User.go
+++ backend/server/Models/User.go
@@ -7,3 +7,68 @@ type User struct {
 	Email     string `json:"email"`
 	LastLogin string `json:"last"`
 }
+
+/*
+Constructor Function for User
+*/
+func NewUser(userID int64, username string, password string, email string, lastLogin string) *User {
+	return &User{
+		UserID:    userID,
+		Username:  username,
+		Password:  password,
+		Email:     email,
+		LastLogin: lastLogin,
+	}
+}
+
+/*
+Getter Functions for User
+-User ID: getUserID()
+-Username: getUsername()
+-Password: getPassword()
+-Email: getEmail()
+-Last Login: getLastLogin()
+*/
+func (u *User) GetUserID() int64 {
+	return u.UserID
+}
+
+func (u *User) GetUsername() string {
+	return u.Username
+}
+
+func (u *User) GetPassword() string {
+	return u.Password
+}
+
+func (u *User) GetEmail() string {
+	return u.Email
+}
+
+func (u *User) GetLastLogin() string {
+	return u.LastLogin
+}
+
+/*
+	Setter Functions for User
+
+-Username: setUsername(username string)
+-Password: setPassword(password string)
+-Email: setEmail(email string)
+-Last Login: setLastLogin(lastLogin string)
+*/
+func (u *User) SetUsername(username string) {
+	u.Username = username
+}
+
+func (u *User) SetPassword(password string) {
+	u.Password = password
+}
+
+func (u *User) SetEmail(email string) {
+	u.Email = email
+}
+
+func (u *User) SetLastLogin(lastLogin string) {
+	u.LastLogin = lastLogin
+}
