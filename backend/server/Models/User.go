6938d15670b09c9f3da68af6df514ca01820d269
Rohan Prasad <r.prasad@ufl.edu>
2024-05-18 21:47:16 -0400

    Added listOfTasks to User model

--- backend/server/Models/User.go
+++ backend/server/Models/User.go
@@ -1,11 +1,13 @@
 package Models
 
+//TODO: replace ListofTasks with pointers to Task objects
 type User struct {
-	UserID    int64  `json:"userID"`
-	Username  string `json:"username"`
-	Password  string `json:"password"`
-	Email     string `json:"email"`
-	LastLogin string `json:"last"`
+	UserID      int64  `json:"userID"`
+	Username    string `json:"username"`
+	Password    string `json:"password"`
+	Email       string `json:"email"`
+	LastLogin   string `json:"last"`
+	ListofTasks []Task `json:"tasks"`
 }
 
 /*
@@ -13,11 +15,12 @@ Constructor Function for User
 */
 func NewUser(userID int64, username string, password string, email string, lastLogin string) *User {
 	return &User{
-		UserID:    userID,
-		Username:  username,
-		Password:  password,
-		Email:     email,
-		LastLogin: lastLogin,
+		UserID:      userID,
+		Username:    username,
+		Password:    password,
+		Email:       email,
+		LastLogin:   lastLogin,
+		ListofTasks: []Task{},
 	}
 }
 
@@ -71,4 +74,48 @@ func (u *User) SetEmail(email string) {
 
 func (u *User) SetLastLogin(lastLogin string) {
 	u.LastLogin = lastLogin
+}
+
+/*
+Editing Task List Functions for User
+-Add Task: AddTask(task Task)
+-Remove Task: RemoveTask(task Task)
+-Get Task: GetTask(taskID int64)
+-Get Tasks: GetTasks()
+-Update Task: UpdateTask(task Task)
+*/
+
+func (u *User) AddTask(task Task) {
+	u.ListofTasks = append(u.ListofTasks, task)
+}
+
+func (u *User) RemoveTask(task Task) {
+	for i, t := range u.ListofTasks {
+		if t.getTaskID() == task.getTaskID() {
+			u.ListofTasks = append(u.ListofTasks[:i], u.ListofTasks[i+1:]...)
+			break
+		}
+	}
+}
+
+func (u *User) GetTask(taskID int64) Task {
+	for _, t := range u.ListofTasks {
+		if t.getTaskID() == taskID {
+			return t
+		}
+	}
+	return Task{}
+}
+
+func (u *User) GetTasks() []Task {
+	return u.ListofTasks
+}
+
+func (u *User) UpdateTask(task Task) {
+	for i, t := range u.ListofTasks {
+		if t.getTaskID() == task.getTaskID() {
+			u.ListofTasks[i] = task
+			break
+		}
+	}
 }
