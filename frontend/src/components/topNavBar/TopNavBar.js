b27e6828760d2e92af5521da7a89ac760af306fa
Rohan Prasad <r.prasad@ufl.edu>
2024-05-23 20:21:01 -0400

    NavBar Fixed

--- frontend/src/components/topNavBar/TopNavBar.js
+++ frontend/src/components/topNavBar/TopNavBar.js
@@ -4,14 +4,12 @@ import './TopNavBar.css'; // Import your CSS file for styling
 const TopNavBar = ({userName}) => {
   return (
     <div className="top-nav-bar">
-      <ul>
+
       <div className="user-name">User123</div>
       <div className="buttons-container">
         <div className="home-button"> Home </div>
         <div className="create-button"> Create </div>
       </div>
-
-      </ul>
     </div>
   );
 }
